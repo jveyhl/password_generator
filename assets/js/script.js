50bc46a792f64d5f4886ca6cda42f292bd65bbad
jve <joe.veyhl@gmail.com>
2022-10-28 20:24:31 -0400

    added password generator function and refactored <textarea> HTML element

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ generateBtn.addEventListener("click", writePassword);
 
 // Assignment code here
 
+// Object that will hold all of the char generation functions
+const randomFunction = {
+  isLowerChars: getCharLow,
+  isUpperChars: getCharUp,
+  isNums: getNum,
+  isSpec: getSpecial,
+}
+
 // Generate a random uppercase letter
 function getCharUp() {
   // Get a random int between 0 and 25; uppercase chars go from 65 to 90 in character set
@@ -18,19 +26,18 @@ function getCharUp() {
 function getCharLow() {
   // Get a random int between 0 and 25; lowercase chars go from 97 to 122 in character set
   let char = String.fromCharCode(Math.floor(Math.random()*26+97));
-  console.log(char);
   return char;
 }
 
 // Generte a special character
 function getSpecial() {
-  let specList = "`~!@#$%^&*()_-+={}[]|\\;:'\"<,>.?/".split("");
+  let specList = "`~!@#$%^&*()_-+={}[]|\\;:'\"<,>.?/";
   let specIndex = Math.floor(Math.random()*specList.length);
   let char = specList[specIndex];
   return char;
 }
 
-// Generate a random int from 0 to 9
+// Generate a random int from 0 to 9 for the numerical password criteria
 function getNum() {
   let num = Math.floor(Math.random()*10);
   return num;
@@ -45,8 +52,10 @@ function findLen() {
     alert("Password length must be a number between 8 and 128 characters");
     findLen();
   } else {
-    alert("Select the remaining password criteria in the following prompts...")
+    alert("Select at least one of the remaining password criteria in the following prompts...")
   }
+  // Convert the string from the prompt into a number
+  passLen = parseInt(passLen);
 
   return passLen;
 }
@@ -117,22 +126,51 @@ function findSpec() {
   return answer;
 }
 
-// Make the password 
+// Generate the password 
 function generatePassword() {
+  // Define password var
+  let password = "";
+
   // Prompt the user for password criteria and assign answers to vars
   let passwordLength = findLen();
+  // These four vars hold booleans
   let isLowerChars = findLchar();
-  let IsUpperChars = findUchar();
-  let IsNums = findNum();
+  let isUpperChars = findUchar();
+  let isNums = findNum();
   let isSpec = findSpec();
 
+  // Find nuumber of criteria functions that were chosen
+  const critCount = isLowerChars+isUpperChars+isNums+isSpec;
 
+  // Array of objects with selected criteria as keys; remove any criteria that are false (not selected by user)
+  const critArray = [{isLowerChars}, {isUpperChars}, {isNums}, {isSpec}].filter(item => Object.values(item)[0]);
+  
+  // Get the keys for the criteria that remain
+  let critArrayKeys = [];
+  for (let i = 0; i < critArray.length; i++) {
+    critArrayKeys.push(Object.keys(critArray[i]));
+  }
+  // Change the 2D array into a 1 dimensional array
+  critArrayKeys = critArrayKeys.flat();
 
-  console.log(passwordLength, IsUpperChars, IsNums, isSpec);
+  // Verify at least one char type is selected before proceeding
+  if (critCount === 0) {
+    alert("At least one of the password criteria must be selected.")
+    return;
+  }
+  
+  // Loop over array and call the char generator functions
+  for (let i = 0; i < passwordLength; i++) {
+    // Get one of the char generator functions randomly
+    let funcName = critArrayKeys[Math.floor(Math.random()*critArrayKeys.length)];
+    // Use the function to generate a random char
+    let randomChar = randomFunction[funcName]();
+    // Append the random char to password
+    password += randomChar;
+  }
+
+  return password;
 }
-
-// Testing:
-generatePassword();
 
 // Write password to the #password input
 function writePassword() {
@@ -141,6 +179,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   // Update the value if <textarea> to the newly generated password
   passwordText.value = password;
+  // passwordText.textContent = password;
 }
 
 
